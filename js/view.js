40df2230d9398046f9a1f8b76f376aca8bc0dd5e
omniboyOk <kachorritou@gmail.com>
2019-09-26 04:47:19 -0300

    Profile overflow fixed, now interface renders properly

--- js/view.js
+++ js/view.js
@@ -36,18 +36,21 @@ class View {
   }
 
   renderProfile(pokemon) {
+    $("#pokedex").removeClass("loading");
     $("#lista").hide();
     $("#load").hide();
     $("#menu").hide();
-    console.log(pokemon.moves)
+    console.log(pokemon)
     const profile = $("#profile");
     profile.html('');
+    //esta variable chequea que exista otro item Type en la lista
+    const type2 = pokemon.types[1] ? pokemon.types[1].type.name : "";
     const content = $(`
     <div class="ui grid basic segment">
         <div class="ui seven wide column">
             <img src="${pokemon.sprites.front_default}" alt="">
             <div class="ui label">${pokemon.types[0].type.name}</div>
-            <div class="ui label">${pokemon.types[1].type.name}</div>
+            <div class="ui label">${type2}</div>
         </div>
         <div class="ui nine wide column">
             <div class="ui label">${pokemon.moves[0].move.name}</div>
@@ -57,6 +60,14 @@ class View {
         </div>
     </div>
     `);
+    const cancel = $(`
+    <button class="ui red basic button" id="back_button">
+    <i class="ui red cancel icon"></i>
+    </button>`);
+    cancel.click(()=>{
+      app.view.showContent()
+    });
+    content.append(cancel);
     profile.append(content).hide().fadeIn(1000)
   }
 
@@ -78,14 +89,16 @@ class View {
   startLoading = () => {
     $("#lista").hide();
     $("#menu").hide();
+    $("#profile").hide();
     $("#load").fadeIn();
     $("#pokedex").addClass("loading");
   };
 
   showContent = () => {
     $("#load").hide();
-    $("#menu").fadeIn();
-    $("#lista").fadeIn();
+    $("#profile").hide();
+    $("#menu").fadeIn(800);
+    $("#lista").fadeIn(800);
     $("#pokedex").removeClass("loading");
   };
 
